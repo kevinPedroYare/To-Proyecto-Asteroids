f6a086239adc666d6e5133f06c6af28aef63034e
Eduard1610 <edurando@unsa.edu.pe>
2023-12-18 11:17:12 -0500

    Reemplazando caracteres ascii por simbolos

--- asteroids.cpp
+++ asteroids.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+
 #include <iostream>
 #include <conio.h>
 
@@ -74,20 +75,20 @@ void pintar()
 	//lineas horizontales
 	for(int i = 2; i < 78; i++)
 	{
-		gotoxy(i,3); printf("%c", 205);
-		gotoxy(i,23); printf("%c", 205);
+		gotoxy(i,3); printf_s("═");
+		gotoxy(i,23); printf_s("═");
 	}
 	//lineas verticales
 	for(int i = 4; i < 23; i++)
 	{
-		gotoxy(2,i); printf("%c",186);
-		gotoxy(77,i); printf("%c",186);
+		gotoxy(2,i); printf_s("║");
+		gotoxy(77,i); printf_s("║");
 	}
 	//esquinas
-	gotoxy(2,3); printf("%c",201);
-	gotoxy(2,23); printf("%c",200);
-	gotoxy(77,3); printf("%c",187);
-	gotoxy(77,23); printf("%c",188);
+	gotoxy(2,3); printf_s("╔");
+	gotoxy(2,23); printf_s("╚");
+	gotoxy(77,3); printf_s("╗");
+	gotoxy(77,23); printf_s("╝");
 }
 
 //Explosion
