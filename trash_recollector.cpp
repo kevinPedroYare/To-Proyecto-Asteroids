ec8493319e736f0e6f607f081b0fd6464dbfe1a4
kevinPedroYare <kyare@unsa.edu.pe>
2023-12-21 15:13:03 -0500

    Se volvio a modificar ahora en juego para que aparesca todo correctamente pero no funciona bien el sistema de conteo de score

--- trash_recollector.cpp
+++ trash_recollector.cpp
@@ -1,3 +1,4 @@
+//Librerias utilizadas
 #include <windows.h>
 #include <iostream>
 #include <conio.h>
@@ -11,6 +12,17 @@
 
 using namespace std;
 
+int num_vidas = 3;
+int corazones = 3;
+int ix = 35; // x inicial
+int iy = 19; // y inicial
+int i,v;
+int score = 0;
+int repeticion = 0, nivel = 1;
+bool condicion = false;
+const int dimx=78, dimy=23;
+
+
 void ubicar(int x, int y) {
     HANDLE hCon;
     COORD dwPos;
@@ -22,18 +34,56 @@ void ubicar(int x, int y) {
     SetConsoleCursorPosition(hCon, dwPos);
 }
 
-void barra_salud(int n) {
-    ubicar(72, 1);
-    printf(" ");
-    ubicar(73, 1);
-    printf(" ");
-    ubicar(74, 1);
-    printf(" ");
+void showConsoleCursor(bool showFlag) {
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    for (int v = 0; v < n; v++) {
-        ubicar(72 + v, 1);
-        printf("%c", 3);
-    }
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = showFlag;
+    SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+void vidas(int vi)
+{
+	ubicar(2,1); printf("VIDAS %d", vi);
+}
+
+void score_view(int s)
+{
+	ubicar(2,25); printf("SCORE %d", s);
+}
+
+void barra_salud(int n)
+{
+	ubicar(72,1); printf(" ");
+	ubicar(73,1); printf(" ");
+	ubicar(74,1); printf(" ");
+	
+	for(v = 0; v < n; v++){
+		ubicar(72+v,1); 
+		printf("%c", 3);
+	}
+}
+
+void dibujar_borde()
+{
+	//lineas horizontales
+	for(int i = 2; i < dimx; i++)
+	{
+		ubicar(i,3); printf_s("%c", 205);
+		ubicar(i,23); printf_s("%c", 205);
+	}
+	//lineas verticales
+	for(int i = 4; i < dimy; i++)
+	{
+		ubicar(2,i); printf_s("%c", 186);
+		ubicar(77,i); printf_s("%c", 186);
+	}
+	//esquinas
+	ubicar(2,3); printf_s("%c", 201);
+	ubicar(2,23); printf_s("%c", 200);
+	ubicar(77,3); printf_s("%c", 187);
+	ubicar(77,23); printf_s("%c", 188);
 }
 
 class Elemento {
@@ -52,16 +102,14 @@ public:
 
     void borrar() {
         ubicar(x, y);
-        printf("       ");
+        printf("     ");
     }
 };
 
 class Nave : public Elemento {
 public:
-    int vidas;
-    int corazones;
 
-    Nave(int x, int y) : Elemento(x, y, "\\-----/"), vidas(3), corazones(3) {}
+    Nave(int x, int y) : Elemento(x, y, "\\____/") {}
 
     void mover(int direccion) {
         borrar();
@@ -71,7 +119,7 @@ public:
                 if (x > 4) x -= 2;
                 break;
             case DERECHA:
-                if (x < 67) x += 2;  // Se ajusta el límite para que la nave no desaparezca en el borde derecho
+                if (x < 70) x += 2;  // Se ajusta el límite para que la nave no desaparezca en el borde derecho
                 break;
             case ARRIBA:
                 if (y > 4) y--;
@@ -80,25 +128,10 @@ public:
                 if (y < 19) y++;
                 break;
         }
-
         dibujar();
     }
 
-    void impacto() {
-        corazones--;
-        barra_salud(corazones);
-        printf("\a"); // sonido de impacto
-    }
-
-    void reset() {
-        vidas--;
-        corazones = 3;
-        ubicar(2, 1);
-        printf("VIDAS %d", vidas);
-        barra_salud(corazones);
-    }
 };
-
 class Obstaculo : public Elemento {
 public:
     Obstaculo(int x, int y) : Elemento(x, y, "P") {}
@@ -108,7 +141,7 @@ public:
 
         if (y > 20) {
             y = 4;
-            x = (rand() % 60) + 6;
+            x = (rand() % 67) + 6;
         }
 
         y++;
@@ -125,7 +158,7 @@ public:
 
         if (y > 20) {
             y = 4;
-            x = (rand() % 60) + 6;
+            x = (rand() % 67) + 6;
         }
 
         y++;
@@ -133,50 +166,20 @@ public:
     }
 };
 
-void showConsoleCursor(bool showFlag) {
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    CONSOLE_CURSOR_INFO cursorInfo;
-    GetConsoleCursorInfo(out, &cursorInfo);
-    cursorInfo.bVisible = showFlag;
-    SetConsoleCursorInfo(out, &cursorInfo);
-}
-
-void dibujar_borde() {
-    for (int i = 2; i <= 77; i++) {  // Se ajusta la condición para que incluya el borde derecho
-        ubicar(i, 3);
-        printf_s("%c", 205);
-        ubicar(i, 23);
-        printf_s("%c", 205);
-    }
-
-    for (int i = 4; i < 24; i++) {
-        ubicar(2, i);
-        printf_s("%c", 186);
-        ubicar(77, i);
-        printf_s("%c", 186);
-    }
-
-    ubicar(2, 3);
-    printf_s("%c", 201);
-    ubicar(2, 23);
-    printf_s("%c", 200);
-    ubicar(77, 3);
-    printf_s("%c", 187);
-    ubicar(77, 23);
-    printf_s("%c", 188);
-}
-
 void jugar(Nave& nave, Obstaculo& obs1, Obstaculo& obs2, Obstaculo& obs3, Obstaculo& obs4, Basura& bas1, Basura& bas2, Basura& bas3, Basura& bas4) {
-    obs1.mover();
-    obs2.mover();
-    obs3.mover();
-    obs4.mover();
+    //rutina asteroides
+	obs1.mover();
+	obs2.mover();
+	obs3.mover();
+	obs4.mover();
 
-    bas1.mover();
-    bas2.mover();
-    bas3.mover();
-    bas4.mover();
+	//rutina basura
+	bas1.mover();
+	bas2.mover();
+	bas3.mover();
+	bas4.mover();
+
+	nave.dibujar();
 
     if (kbhit()) {
         unsigned char tecla = getch();
@@ -193,30 +196,44 @@ void jugar(Nave& nave, Obstaculo& obs1, Obstaculo& obs2, Obstaculo& obs3, Obstac
     if ((obs1.x > nave.x && obs1.x < nave.x + 6 && obs1.y == nave.y - 1) ||
         (obs2.x > nave.x && obs2.x < nave.x + 6 && obs2.y == nave.y - 1) ||
         (obs3.x > nave.x && obs3.x < nave.x + 6 && obs3.y == nave.y - 1) ||
-        (obs4.x > nave.x && obs4.x < nave.x + 6 && obs4.y == nave.y - 1)) {
-        nave.impacto();
-    }
+        (obs4.x > nave.x && obs4.x < nave.x + 6 && obs4.y == nave.y - 1)) {  
+    }{
+		corazones--;
+		barra_salud(corazones);
+		printf("\a"); // sonido de impacto
+	}
 
     if ((bas1.x > nave.x && bas1.x < nave.x + 6 && bas1.y == nave.y - 1) ||
         (bas2.x > nave.x && bas2.x < nave.x + 6 && bas2.y == nave.y - 1) ||
         (bas3.x > nave.x && bas3.x < nave.x + 6 && bas3.y == nave.y - 1) ||
         (bas4.x > nave.x && bas4.x < nave.x + 6 && bas4.y == nave.y - 1)) {
         printf("\a");
-    }
+    }{
+		score++;
+		score_view(score);
+	}
 
-    nave.dibujar();
+	if(corazones == 0){
+		num_vidas--;
+		vidas(num_vidas);
+		corazones = 3;
+		barra_salud(corazones);
+	}
 }
 
 int main() {
     showConsoleCursor(false);
     dibujar_borde();
+	vidas(num_vidas);
+	score_view(score);
+	barra_salud(corazones);
 
     Nave nave(35, 19);
-    Obstaculo obs1(12, 8), obs2(17, 12), obs3(58, 6), obs4(70, 9);
-    Basura bas1(10, 6), bas2(14, 11), bas3(45, 7), bas4(65, 10);  // Ajuste de la posición de bas4
-    int velocidad = 60;  // Ajuste la velocidad a su preferencia (menos es más rápido)
+	Obstaculo obs1(10, 4), obs2(20, 4), obs3(58, 6), obs4(70, 9);
+    Basura bas1(10, 6), bas2(14, 11), bas3(45, 7), bas4(65, 10);
+    int velocidad = 400;  // Ajuste la velocidad a su preferencia (menos es más rápido)
 
-    while (nave.vidas > 0) {
+    while (num_vidas > 0) {
         jugar(nave, obs1, obs2, obs3, obs4, bas1, bas2, bas3, bas4);
         this_thread::sleep_for(chrono::milliseconds(velocidad));  // Añadida pausa para controlar la velocidad
     }
