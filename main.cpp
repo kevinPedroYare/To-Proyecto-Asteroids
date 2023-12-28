5b13a9d08360d2a128b0d121faa6a5da0f6f7fb0
root <root@tofinal.myguest.virtualbox.org>
2023-12-28 14:50:54 -0500

    agregando obstaculos y sonido

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ const float SPAWN_INTERVAL = 2.0f;
 class Game {
 public:
     Game();
-    ~Game(); // Agregado para liberar recursos
+    ~Game();
     void run();
 
 private:
@@ -37,6 +37,8 @@ private:
     sf::Sprite player;
 
     sf::Texture obstacleTexture;
+    sf::Texture obstacleTexture1;  // Nueva textura para un tipo de obstáculo
+    sf::Texture obstacleTexture2;  // Nueva textura para otro tipo de obstáculo
     std::vector<sf::Sprite> obstacles;
 
     sf::Font font;
@@ -51,22 +53,19 @@ private:
 
 Game::Game()
     : window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "FinalTO"),
-    playerTexture(), player(), obstacleTexture(), obstacles(),
-    font(), scoreText(), score(0), hitBuffer(), hitSound(), spawnTimer() {
-    // Cargar texturas
-    if (!playerTexture.loadFromFile("tacho.png") || !obstacleTexture.loadFromFile("basura.png")) {
-        // Manejar el error al cargar texturas
+      playerTexture(), player(), obstacleTexture(), obstacleTexture1(), obstacleTexture2(), obstacles(),
+      font(), scoreText(), score(0), hitBuffer(), hitSound(), spawnTimer()
+{
+    if (!playerTexture.loadFromFile("tacho.png") || !obstacleTexture.loadFromFile("basura.png") ||
+        !obstacleTexture1.loadFromFile("tacho.png") || !obstacleTexture2.loadFromFile("tacho.png")) {
         std::cerr << "Error al cargar texturas." << std::endl;
         std::exit(EXIT_FAILURE);
     }
 
-    // Inicializar jugador
     player.setTexture(playerTexture);
     player.setPosition(WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT - 50.0f);
 
-    // Inicializar texto
     if (!font.loadFromFile("Futura Condensed.ttf")) {
-        // Manejar el error al cargar la fuente
         std::cerr << "Error al cargar la fuente." << std::endl;
         std::exit(EXIT_FAILURE);
     }
@@ -75,9 +74,7 @@ Game::Game()
     scoreText.setFillColor(sf::Color::White);
     scoreText.setPosition(10.0f, 10.0f);
 
-    // Inicializar sonido
     if (!hitBuffer.loadFromFile("beep.wav")) {
-        // Manejar el error al cargar el sonido
         std::cerr << "Error al cargar el sonido." << std::endl;
         std::exit(EXIT_FAILURE);
     }
@@ -85,8 +82,6 @@ Game::Game()
 }
 
 Game::~Game() {
-    // Liberar recursos
-    // No es necesario en este caso ya que los objetos sf::Texture y sf::SoundBuffer se encargan automáticamente de liberar sus recursos.
 }
 
 void Game::run() {
@@ -102,18 +97,15 @@ void Game::processEvents() {
     while (window.pollEvent(event)) {
         if (event.type == sf::Event::Closed) {
             window.close();
-        }
-        else if (event.type == sf::Event::KeyPressed) {
+        } else if (event.type == sf::Event::KeyPressed) {
             handlePlayerInput(event.key.code, true);
-        }
-        else if (event.type == sf::Event::KeyReleased) {
+        } else if (event.type == sf::Event::KeyReleased) {
             handlePlayerInput(event.key.code, false);
         }
     }
 }
 
 void Game::update(sf::Time deltaTime) {
-    // Actualizar posición del jugador
     sf::Vector2f movement(0.0f, 0.0f);
     if (sf::Keyboard::isKeyPressed(IZQUIERDA) && player.getPosition().x > 0) {
         movement.x -= PLAYER_SPEED;
@@ -124,66 +116,77 @@ void Game::update(sf::Time deltaTime) {
 
     player.move(movement * deltaTime.asSeconds());
 
-    // Actualizar obstáculos
     for (auto it = obstacles.begin(); it != obstacles.end();) {
         it->move(0.0f, OBSTACLE_SPEED * deltaTime.asSeconds());
 
-        // Verificar colisiones con el jugador
         if (it->getGlobalBounds().intersects(player.getGlobalBounds())) {
             hitSound.play();
             score++;
             spawnTimer = sf::seconds(SPAWN_INTERVAL);
-            it = obstacles.erase(it); // Utilizar el valor devuelto por erase para obtener el siguiente iterador válido
-        }
-        else {
-            // Verificar si los obstáculos han llegado al final de la pantalla
+            it = obstacles.erase(it);
+        } else {
             if (it->getPosition().y > WINDOW_HEIGHT) {
                 score--;
                 hitSound.play();
-                it = obstacles.erase(it); // Utilizar el valor devuelto por erase para obtener el siguiente iterador válido
-            }
-            else {
+                sf::SoundBuffer fallBuffer;
+                sf::Sound fallSound;
+                if (fallBuffer.loadFromFile("beep.wav")) {
+                    fallSound.setBuffer(fallBuffer);
+                    fallSound.play();
+                }
+                it = obstacles.erase(it);
+            } else {
                 ++it;
             }
         }
     }
 
-    // Controlar la frecuencia de aparición de obstáculos
     spawnTimer += deltaTime;
     if (spawnTimer >= sf::seconds(SPAWN_INTERVAL)) {
         spawnObstacle();
         spawnTimer = sf::Time::Zero;
     }
 
-    // Actualizar el texto del puntaje
     scoreText.setString("Puntaje: " + std::to_string(score));
 }
 
 void Game::render() {
     window.clear();
 
-    // Dibujar jugador
     window.draw(player);
 
-    // Dibujar obstáculos
     for (const auto& obstacle : obstacles) {
         window.draw(obstacle);
     }
 
-    // Dibujar texto de puntaje
     window.draw(scoreText);
 
     window.display();
 }
 
 void Game::spawnObstacle() {
-    sf::Sprite obstacle(obstacleTexture);
+    sf::Sprite obstacle;
+    int obstacleType = rand() % 3;  // Asumiendo que hay 3 tipos de obstáculos en total
+
+    switch (obstacleType) {
+    case 0:
+        obstacle.setTexture(obstacleTexture);
+        break;
+    case 1:
+        obstacle.setTexture(obstacleTexture1);
+        break;
+    case 2:
+        obstacle.setTexture(obstacleTexture2);
+        break;
+    default:
+        break;
+    }
+
     obstacle.setPosition(static_cast<float>(rand() % (WINDOW_WIDTH - static_cast<int>(obstacle.getGlobalBounds().width))), 0.0f);
     obstacles.push_back(obstacle);
 }
 
 void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
-    // Manejar la entrada del jugador (por ejemplo, teclas de disparo, si es que se implementa)
 }
 
 int main() {
